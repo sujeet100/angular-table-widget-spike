20b3880304a80961cf1ee35199e0b4f91842806b
Rashmi <rparab@equalexperts.com>
2014-12-02 18:35:28 +0530

    sorting functionality working on all columns sortpredicate is configurable and can be send as an attribute to directive removed JS based approach to generate markup and used ng-repeat to generate the table

--- app.js
+++ app.js
@@ -2,38 +2,57 @@ angular.module("widget", [])
     .controller("rootController", ["$scope",
         function($scope) {
        	$scope.students = [];
-        $scope.students.push({name:'sujit', age:24, city:'pune', marks:59});
-        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:43});
+        $scope.students.push({name:'sujit', age:25, city:'pune', marks:59});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43}); 
+        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:43});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:98});
+        $scope.students.push({name:'rashmi', age:20, city:'chennai', marks:43});
+        $scope.students.push({name:'sana', age:18, city:'mumbai', marks:100});
+        $scope.students.push({name:'arti', age:18, city:'lonavla', marks:0});
+        $scope.students.push({name:'rashmi', age:0, city:'mumbai', marks:-10});
+        $scope.students.push({name:'rashmi', age:5 , city:'mumbai', marks:50});
+        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:150});
+        $scope.students.push({name:'leena', age:0, city:'lonavla', marks:43});
+        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:37});
+        $scope.students.push({name:'rashmi', age:18, city:'mumbai', marks:54});
+        $scope.students.push({name:'rashmi', age:19, city:'nagpur', marks:98});
+        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:78});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43});
+        $scope.students.push({name:'rashmi', age:23, city:'saswad', marks:42});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:49});
+        
         }
     ])
-    .directive("table", function(){
+    .directive("table", ['$compile', function($compile){
     	return{
     		restrict: 'AE',
     		scope:{
     			data:'=',
                 columns:'=',
-                headers:'='
+                defaultsortpredicate: '=',
     		},
     		link: function(scope, element, attr){
-    			var tableElement = [];
-                tableElement.push('<table>');
 
-                var columns = scope.columns.split(",");
-                var headers = scope.headers.split(",");
+                    var tableElement = [];
+                    console.log(scope.defaultsortpredicate);
+                    scope.sortpredicate=scope.defaultsortpredicate
+                    tableElement.push('<style type="text/css">span{margin:30px;}</style>');
+                    tableElement.push('<span ng-repeat="head in columns">');
+                    tableElement.push('<a href="" ng-click="changePredicate(head.predicate)"><strong>{{head.header}}</strong></a></span>');
+                     tableElement.push('<div ng-repeat="student in data  |orderBy:sortpredicate:reverse">');
+                         tableElement.push('<span ng-repeat="column in columns">');
+                         tableElement.push('{{student[column.field]}}</span>');
+                     tableElement.push('</div>');
 
-                    for(k=0;k<headers.length;k++){
-                        tableElement.push('<th>'+headers[k].trim()+'</th>');
-                    }
+                     scope.changePredicate= function(predicate){
+                        console.log(scope.sortpredicate);
+                        scope.sortpredicate=predicate;
+                        scope.reverse=!scope.reverse;
+                     }   
 
-                for(i=0; i<scope.data.length; i++){ 
-                    tableElement.push('<tr>');
-                    for(j=0;j<columns.length;j++){
-                        tableElement.push('<td>'+scope.data[i][columns[j].trim()]+'</td>');
-                    }
-                    tableElement.push('</tr>');
-                }
-                tableElement.push("</table>");
-    			element.append(tableElement.join(""));
+
+                    var content = $compile(tableElement.join(""))(scope);
+                    element.append(content);
     		}
     	}
-    });
+    }]);
