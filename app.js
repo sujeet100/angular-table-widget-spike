ef9520d587391c919ea7bc91ae048ae101c79cfa
sujeet100 <sujeet100@gmail.com>
2014-12-03 15:33:41 +0530

    implemented show icon based on a field

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ angular.module("widget", [])
     .controller("rootController", ["$scope",
         function($scope) {
        	$scope.students = [];
-        $scope.students.push({name:'sujit', age:25, city:'pune', marks:59});
-        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43}); 
-        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:43});
-        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:98});
-        $scope.students.push({name:'rashmi', age:20, city:'chennai', marks:43});
-        $scope.students.push({name:'sana', age:18, city:'mumbai', marks:100});
-        $scope.students.push({name:'arti', age:18, city:'lonavla', marks:0});
-        $scope.students.push({name:'rashmi', age:0, city:'mumbai', marks:-10});
-        $scope.students.push({name:'rashmi', age:5 , city:'mumbai', marks:50});
-        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:150});
-        $scope.students.push({name:'leena', age:0, city:'lonavla', marks:43});
-        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:37});
-        $scope.students.push({name:'rashmi', age:18, city:'mumbai', marks:54});
-        $scope.students.push({name:'rashmi', age:19, city:'nagpur', marks:98});
-        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:78});
-        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43});
-        $scope.students.push({name:'rashmi', age:23, city:'saswad', marks:42});
-        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:49});
+        $scope.students.push({name:'sujit', age:25, city:'pune', marks:59, fail: true});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43, fail: false}); 
+        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:43, fail: true});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:98, fail: true});
+        $scope.students.push({name:'rashmi', age:20, city:'chennai', marks:43, fail: false});
+        $scope.students.push({name:'sana', age:18, city:'mumbai', marks:100, fail: true});
+        $scope.students.push({name:'arti', age:18, city:'lonavla', marks:0, fail: true});
+        $scope.students.push({name:'rashmi', age:0, city:'mumbai', marks:-10, fail: false});
+        $scope.students.push({name:'rashmi', age:5 , city:'mumbai', marks:50, fail: true});
+        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:150, fail: false});
+        $scope.students.push({name:'leena', age:0, city:'lonavla', marks:43, fail: true});
+        $scope.students.push({name:'leena', age:0, city:'mumbai', marks:37, fail: true});
+        $scope.students.push({name:'rashmi', age:18, city:'mumbai', marks:54, fail: false});
+        $scope.students.push({name:'rashmi', age:19, city:'nagpur', marks:98, fail: true});
+        $scope.students.push({name:'rashmi', age:28, city:'mumbai', marks:78, fail: false});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:43, fail: false});
+        $scope.students.push({name:'rashmi', age:23, city:'saswad', marks:42, fail: false});
+        $scope.students.push({name:'rashmi', age:23, city:'mumbai', marks:49, fail: true});
         
         }
     ])
@@ -38,7 +38,8 @@ angular.module("widget", [])
                     var tableElement = [];
                     scope.sortpredicate=scope.defaultSortPredicate
                     scope.rowsVisible = scope.rowsPerPage;
-                    tableElement.push('<style type="text/css">span{margin:30px;}</style>');
+                    tableElement.push('<style type="text/css">span{margin:30px;}span.fail-true{background-image:url(\'http://reddot.com.au/wp-content/uploads/Looking-Left-Up-Face1.jpg\');background-size:15 15;background-repeat: no-repeat;padding-left: 15;}</style>');
+                    tableElement.push('<style type="text/css">span.fail-false{background-image:url(\'http://clickmedix.com/wp-content/uploads/2011/12/504px-Android_robot-252x300.png\');background-size:15 15;background-repeat: no-repeat;padding-left: 15;}</style>');
                    
                    //headers
                     tableElement.push('<span ng-repeat="column in columns">');
@@ -49,7 +50,13 @@ angular.module("widget", [])
                     //rows
                     tableElement.push('<div ng-repeat="student in data | orderBy:sortpredicate:reverse | limitTo:rowsVisible">');
                         tableElement.push('<span ng-repeat="column in columns">');
-                        tableElement.push('{{student[column.field]}}</span>');
+                        tableElement.push('<span ng-if="column.showIconField" class="{{column.showIconField}}-{{student[column.showIconField]}}">');
+                        tableElement.push('{{student[column.field]}}');
+                        tableElement.push('</span>');
+                        tableElement.push('<span ng-if="!column.showIconField">');
+                        tableElement.push('{{student[column.field]}}');
+                        tableElement.push('</span>');
+                        tableElement.push('</span>');
                     tableElement.push('</div>');
 
                     //pagination
